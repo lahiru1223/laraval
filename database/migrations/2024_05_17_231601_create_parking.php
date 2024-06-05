ffe2c3fc0c696da2e3c16e299112b437200bcbcd
lahiru.ubunutu <lahiru.ubuntu@gmail.com>
2024-06-05 20:08:38 +0530

    after migrating to ubuntu

--- database/migrations/2024_05_17_231601_create_parking.php
+++ database/migrations/2024_05_17_231601_create_parking.php
@@ -1,3 +1,7 @@
+
+
+
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
@@ -11,7 +15,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('parking', function (Blueprint $table) {
+        Schema::create('parkings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('parking_spot_id')->constrained()->onDelete('cascade');
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) { // Change the number to generate more or fewer records
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => static::$password ??= Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);   
+    
+            } 
+    }
+}
